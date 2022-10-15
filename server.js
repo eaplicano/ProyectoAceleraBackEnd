cc65c5dddfbdf061666b2af3b1b691b99e8cfb61
eaplicano <eliud.aplicano@unah.hn>
2022-10-15 13:24:46 -0600

    Otro commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-'use strict'
+"use strict";
 
-var app = require('./app'),
-	server = app.listen(app.get('port'), () => {
-		console.log(`Iniciando Express en el puerto ${app.get('port')}`)
-	})
+var app = require("./app"),
+  server = app.listen(app.get("port"), () => {
+    console.log(`Iniciando Express en el puerto ${app.get("port")}`);
+  });
